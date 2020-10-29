00377339174316b604a1df8ce3c7aa9f3996a7a4
Jonh28-02 <jriverag1900@alumno.ipn.mx>
2020-10-28 18:18:35 -0600

    HOLA MUNDO

---
+++ HOLA MUNDO/HolaMundo.java
@@ -0,0 +1,10 @@
+class HolaMundo {
+    public static void main(String[]args){
+
+    String nombre ="HolaMundoxd";
+
+    System.out.println ("Di Holamundo ploz");
+    System.out.println (nombre);
+
+    }
+}
